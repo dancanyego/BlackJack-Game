cdc99daa245f1a06f8adf647a0446a0db7d88327
Dancan Yego <dancankipchumba76@gmail.com>
2023-04-03 22:42:23 +0300

    Deck Class

--- game.py
+++ game.py
@@ -5,3 +5,49 @@ Created on Mon Apr  3 21:43:41 2023
 @author: danca
 """
 
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+playing = True
+
+# Card Class
+
+class Card():
+    
+    def __init__(self,suit,rank):
+        
+        self.rank = rank
+        self.suit = suit
+        
+    def __str__(self):
+        return self.rank + " Of " + self.suit
+
+    
+# Deck Class To Store The cards
+
+class Deck():
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+                
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return " The Deck has : " + deck_comp
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+    
+test_deck = Deck()
+print(test_deck)
